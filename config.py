693de582ba65d75fa5ed42088c8c9fb342e8dc30
aapotapo <s.budaev@fun-box.ru>
2021-04-14 13:27:17 +0200

    Добавлена конфигурация ключей через переменные окружения

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+TOKEN = os.environ.get('TELEGRAM_TOKEN')
+EXCHANGEAPI = 'https://openexchangerates.org/api/'
+CRYPTOCOMPAREAPI_KEY = os.environ.get('CRYPTOCOMPAREAPI_KEY')
+CRYPTOCOMPAREAPI = 'https://min-api.cryptocompare.com/data/'
